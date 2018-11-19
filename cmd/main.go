6a4dbf3893f94175b8772f01365abc999435e518
olebjorn <olebgr@stud.ntnu.no>
2018-11-19 14:44:04 +0100

    new DB for skiers

--- cmd/main.go
+++ cmd/main.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"net/http"
+	"time"
 )
 
 var dbTracks SnowTrackStorage
 var dbSkiers SkiersStorage
 var Skiers Skier
 var SnowTracks SnowTrack
+var startTime time.Time
 
 func main() {
+	startTime = time.Now()
 	dbTracks = &SnowTracksMongoDB{"mongodb://olebgr:password1@ds261253.mlab.com:61253/snowtracks",
 	"snowtracks", "tracks"}
-	dbSkiers = &SkiersMongoDB{"mongodb://olebgr:password1@ds261253.mlab.com:61253/snowtracks",
+	dbSkiers = &SkiersMongoDB{"mongodb://olebgr:password1@mongodb@ds211774.mlab.com:11774/skiers",
 		"skiers", "skiers"}
 
 	dbTracks.Init()
 	http.HandleFunc("/snowtrack/track/", trackHandler)
 	http.HandleFunc("/snowtrack/skier/", skierHandler)
+	http.HandleFunc("/snowtracks/api", apiHandler)
 	http.ListenAndServe(getPort(), nil)
 
 }

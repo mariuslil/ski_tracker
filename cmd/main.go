56a3640a951f6f85512f1ae6543dd3bfe51c81ae
Marius <marlil@stud.ntnu.no>
2018-11-19 14:37:22 +0100

    minor

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"net/http"
+	"time"
 )
 
 var dbTracks SnowTrackStorage
 var dbSkiers SkiersStorage
 var Skiers Skier
 var SnowTracks SnowTrack
+var startTime time.Time
 
 func main() {
+	startTime = time.Now()
 	dbTracks = &SnowTracksMongoDB{"mongodb://olebgr:password1@ds261253.mlab.com:61253/snowtracks",
 	"snowtracks", "tracks"}
 	dbSkiers = &SkiersMongoDB{"mongodb://olebgr:password1@ds261253.mlab.com:61253/snowtracks",
